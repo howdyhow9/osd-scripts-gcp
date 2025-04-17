055190aad53ae28a1c18cba75534dc92e25e29d0
howdyhow9 <163223182+howdyhow9@users.noreply.github.com>
2025-04-17 00:25:29 -0400

    cow2

--- IotEventsHudiCoWMoR.py
+++ IotEventsHudiCoWMoR.py
@@ -53,37 +53,21 @@ hudi_options_cow = {
     "hoodie.datasource.write.recordkey.field": "uuid",
     "hoodie.datasource.write.partitionpath.field": "",  # No partitioning
     "hoodie.datasource.write.precombine.field": "ts",
-    "hoodie.datasource.write.operation": "upsert",
     "hoodie.datasource.hive_sync.enable": "true",
     "hoodie.datasource.hive_sync.database": "kafka_hudi",
     "hoodie.datasource.hive_sync.table": table_name_cow,
     "hoodie.datasource.hive_sync.partition_fields": ""
 }
 
-# Hudi configurations for new MOR table
-base_path_mor = "gs://osd-data/kafka_hudi.db/iot_events_mor"
-table_name_mor = "iot_events_mor"
-hudi_options_mor = {
-    "hoodie.table.name": table_name_mor,
-    "hoodie.datasource.write.recordkey.field": "uuid",
-    "hoodie.datasource.write.partitionpath.field": "",
-    "hoodie.datasource.write.precombine.field": "ts",
-    "hoodie.datasource.write.operation": "upsert",
-    "hoodie.datasource.hive_sync.enable": "true",
-    "hoodie.datasource.hive_sync.database": "kafka_hudi",
-    "hoodie.datasource.hive_sync.table": table_name_mor,
-    "hoodie.datasource.hive_sync.partition_fields": ""
-}
-
 # Function to write to Hudi
-def write_hudi(df, base_path, hudi_options, table_type, operation="upsert", mode="append"):
-    df.write.format("hudi") \
-        .options(**hudi_options, **{
-        "hoodie.datasource.write.table.type": table_type,
-        "hoodie.datasource.write.operation": operation
-    }) \
-        .mode(mode) \
-        .save(base_path)
+def write_hudi(df, base_path, hudi_options, table_type, operation="upsert  mode="append"):
+df.write.format("hudi") \
+    .options(**hudi_options, **{
+    "hoodie.datasource.write.table.type": table_type,
+    "hoodie.datasource.write.operation": operation
+}) \
+    .mode(mode) \
+    .save(base_path)
 
 # Function to read from Hudi
 def read_hudi(base_path, query_type="snapshot", begin_time=None):
@@ -91,8 +75,6 @@ def read_hudi(base_path, query_type="snapshot", begin_time=None):
     if query_type == "incremental":
         reader = reader.option("hoodie.datasource.query.type", "incremental") \
             .option("hoodie.datasource.read.begin.instanttime", begin_time)
-    elif query_type == "read_optimized":
-        reader = reader.option("hoodie.datasource.query.type", "read_optimized")
     return reader.load(base_path)
 
 # --- Test COW (iot_events) ---
@@ -109,7 +91,7 @@ update_data = [
     ("IoT_03", "2025-04-17 04:00:00", 70.00, 4, 17, 4, 0, "2025/04/17", "IoT_03_2025-04-17_04:00:00", "IoT_03_2025-04-17_04:00:00", "2025-04-17 04:00:00", "2025-04-17 04:06:00", 2)
 ]
 update_df = spark.createDataFrame(update_data, schema)
-write_hudi(update_df, base_path_cow, hudi_options_cow, "COPY_ON_WRITE")
+write_hudi(update_df, base_path_cow, hudi_options_cow, "COPY_ON_WRITE", operation="upsert")
 print("COW Upsert:")
 read_hudi(base_path_cow).select("uuid", "ts", "consumption", "date").show()
 
@@ -125,34 +107,6 @@ commits = spark.read.format("hudi").load(base_path_cow).select("_hoodie_commit_t
 begin_time = min([row._hoodie_commit_time for row in commits])
 print("COW Incremental Query:")
 read_hudi(base_path_cow, query_type="incremental", begin_time=begin_time).select("uuid", "ts", "consumption", "date").show()
-
-# --- Test MOR (iot_events_mor) ---
-print("Testing MOR on iot_events_mor")
-
-# Test 5: Insert (MOR)
-write_hudi(df, base_path_mor, hudi_options_mor, "MERGE_ON_READ", operation="insert", mode="overwrite")
-print("MOR Insert:")
-read_hudi(base_path_mor).select("uuid", "ts", "consumption", "date").show()
-
-# Test 6: Upsert (MOR)
-write_hudi(update_df, base_path_mor, hudi_options_mor, "MERGE_ON_READ")
-print("MOR Upsert:")
-read_hudi(base_path_mor).select("uuid", "ts", "consumption", "date").show()
-
-# Test 7: Delete (MOR)
-write_hudi(delete_df, base_path_mor, hudi_options_mor, "MERGE_ON_READ", operation="delete")
-print("MOR Delete:")
-read_hudi(base_path_mor).select("uuid", "ts", "consumption", "date").show()
-
-# Test 8: Read-Optimized Query (MOR)
-print("MOR Read-Optimized Query:")
-read_hudi(base_path_mor, query_type="read_optimized").select("uuid", "ts", "consumption", "date").show()
-
-# Test 9: Incremental Query (MOR)
-commits = spark.read.format("hudi").load(base_path_mor).select("_hoodie_commit_time").distinct().collect()
-begin_time = min([row._hoodie_commit_time for row in commits])
-print("MOR Incremental Query:")
-read_hudi(base_path_mor, query_type="incremental", begin_time=begin_time).select("uuid", "ts", "consumption", "date").show()
 
 # Stop Spark session
 spark.stop()
